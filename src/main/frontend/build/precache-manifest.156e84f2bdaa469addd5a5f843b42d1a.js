e6a76df67d13adf139e8f02206eaa8978db1a510
Kim memil <xc918@naver.com>
2022-12-21 18:11:36 +0900

    221221
    - build upload

---
+++ src/main/frontend/build/precache-manifest.156e84f2bdaa469addd5a5f843b42d1a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "632286fe5e76d9aa6217",
+    "url": "/Mushroom_API/static/css/main.466098e8.chunk.css"
+  },
+  {
+    "revision": "632286fe5e76d9aa6217",
+    "url": "/Mushroom_API/static/js/main.632286fe.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/Mushroom_API/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "c2d6b731324c1ef7ad88",
+    "url": "/Mushroom_API/static/js/2.c2d6b731.chunk.js"
+  },
+  {
+    "revision": "b4bf3e9cb0e7ab92dc3b",
+    "url": "/Mushroom_API/static/js/runtime~main.b4bf3e9c.js"
+  },
+  {
+    "revision": "b0a6a2693556296fa674f5e4de61810b",
+    "url": "/Mushroom_API/static/media/MaplestoryBold.b0a6a269.ttf"
+  },
+  {
+    "revision": "fb932d042f385927ba59e9a690538709",
+    "url": "/Mushroom_API/static/media/MaplestoryLight.fb932d04.ttf"
+  },
+  {
+    "revision": "b6d972fbebb667a2bd18bbee9121d7c0",
+    "url": "/Mushroom_API/index.html"
+  }
+];
